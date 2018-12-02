78ae8ea27be5230ba75d99da3c5567a6934faf7a
VirtusDesktopPC <vbo@rastec.dk>
2018-12-02 04:39:05 +0100

    Initial commit

---
+++ listing_types.py
@@ -0,0 +1,12 @@
+# An ad listing can either be 'plus', 'discount' or 'exclusive'
+# The class name of the div container depends on this type
+
+def CONST_PLUS_AD_TYPE():
+    return "row listing listing-plus bb-listing-clickable"
+
+def CONST_PLUS_DISCOUNT_TYPE():
+    return "row listing listing-discount bb-listing-clickable"
+
+
+def CONST_PLUS_EXCLUSIVE_TYPE():
+    return "row listing listing-exclusive bb-listing-clickable"
---
+++ scrape.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import listing_types as AD_TYPE
+
+# URL to scrape
+bmw_url = "https://www.bilbasen.dk/brugt/bil/bmw"
+
+# Open a HTTP request to the URL, and load the response into a variable
+uClient = uReq(bmw_url)
+# Read the HTML response returned into a variable
+page_html = uClient.read()
+# Close the request after reading
+uClient.close()
+
+# Parse the HTML using bs4
+page_soup = soup(page_html, "html.parser")
+
+# Grab the divs containing the cars, currently only 1 page, and currently only the 'plus' listing type
+containers = page_soup.findAll("div", {"class": AD_TYPE.CONST_PLUS_AD_TYPE()})
+print(len(containers))
+
